6b7ebbbdab8b068c6e826b2744d561e63304e749
Manuel Silva <70995042+Manuel800@users.noreply.github.com>
2022-08-26 01:50:50 +0100

    Add files via upload
    Gerador de nome de Banda

---
+++ finalproject.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+#create a greeting for your program.
+#Criar saudação para o seu programa.
+print("Welcome to the Band Name Generator.")
+#Ask the user for the city that they grew up in.
+#Pergunte ao usuário, qual a cidade que ele cresceu?
+city = input("Qual o nome da cidade que você cresceu?\n")
+#Ask the user for the name of a pet.
+#Pergunta ao usuário qual o nome de seu animal.
+pet = input("Qual é o nome do seu animal?\n")
+#Combine a name of their city an pet and show them their band name.
+#combinar um nome de sua cidade e do seu animal e mostra o nome de sua banda.
+print("O nome de sua banda poderia ser "+ city + " " + pet)
